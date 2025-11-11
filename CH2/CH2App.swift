cf44740bb292b66274c82145bb3a3e10753bf7ce
AsadRaza98 <asad.raza940@gmail.com>
2025-11-11 15:09:57 +0100

    Initial version
    Only project was created by now

---
+++ CH2/CH2App.swift
@@ -0,0 +1,17 @@
+//
+//  CH2App.swift
+//  CH2
+//
+//  Created by Asad Raza on 07/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CH2App: App {
+    var body: some Scene {
+        WindowGroup {
+            CameraView()
+        }
+    }
+}
---
+++ CH2/CameraView.swift
@@ -0,0 +1,24 @@
+//
+//  CameraView.swift
+//  CH2
+//
+//  Created by Asad Raza on 07/11/25.
+//
+
+import SwiftUI
+import AVFoundation
+
+class CameraView: UIView {
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
+    
+    /// Convenience wrapper to get layer as its statically known type.
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+        return layer as! AVCaptureVideoPreviewLayer
+    }
+}
+
+#Preview {
+    CameraView()
+}
